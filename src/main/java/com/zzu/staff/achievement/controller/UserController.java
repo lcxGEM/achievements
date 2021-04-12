7f45762a11755946727cdb29593b6da66e184eba
lcx <chenxiligem@163.com>
2021-04-12 17:13:55 +0800

    user

--- src/main/java/com/zzu/staff/achievement/controller/UserController.java
+++ src/main/java/com/zzu/staff/achievement/controller/UserController.java
@@ -3,9 +3,7 @@ package com.zzu.staff.achievement.controller;
 import com.zzu.staff.achievement.entity.User;
 import com.zzu.staff.achievement.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,28 @@ public class UserController {
         return userService.queryAll();
     }
 
+    @GetMapping("queryByDepartment/{id}")
+    public List<User> queryByDepartment(@PathVariable("id")Integer id){
+        return  userService.queryByDepartment(id);
+    }
 
+    @GetMapping("queryById/{id}")
+    public User queryById(@PathVariable("id")Long id){
+        return userService.queryById(id);
+    }
+
+    @PostMapping("insert")
+    public int insert(User user){
+        return userService.insert(user);
+    }
+
+    @PostMapping("update")
+    public int update(User user){
+        return userService.update(user);
+    }
+
+    @GetMapping("deleteById/{id}")
+    public int deleteById(@PathVariable("id")Long id){
+        return userService.deleteById(id);
+    }
 }
